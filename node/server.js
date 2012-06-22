6123c4e5900e3aa63705a10d5164e1515f1c470b
Michal Kuklis <michal.kuklis@gmail.com>
2012-06-22 16:50:47 -0400

    adding create request

--- node/server.js
+++ node/server.js
@@ -1,37 +1,34 @@
-/*
-var express = require('express')
-  ,  socket = require('socket.io')
-  , app = express.createServer();
-
-app.listen(8080);
-app.use(express.bodyParser());
-
-var io = socket.listen(app);
-*/
-
 var http = require('http');
 var backboneio = require('backbone.io');
-
-console.log(backboneio);
+var rest = require('restler');
 var app = http.createServer();
+
 app.listen(3000);
 
 var backend = backboneio.createBackend();
 
 backboneio.middleware.sinatra = function () {
-  return function(rex, res, next) {
+  var uri = 'http://10.0.0.14:4567';
+
+  return function(req, res, next) {
+    //console.log(req);
     var crud = {
       create: function () {
-        console.log("in create...");
+        req.model.node = true;
+        rest.postJson(uri + '/todo', req.model).on('complete', function (data, response) {
+          console.log(data);
+          res.end(data);
+        });
       },
-      read: function () {
-      },
-
+      read: function () {},
       update: function () {
-      },
+        rest.postJson(uri + '/todo', req.model).on('complete', function (data, response) {
+          console.log(data);
+          res.end(data);
+        });
 
-      delete: function () {
-      }
+      },
+      delete: function () {}
     };
 
     if (!crud[req.method]) return next(new Error('Unsuppored method ' + req.method));
@@ -40,30 +37,4 @@ backboneio.middleware.sinatra = function () {
 }
 
 backend.use(backboneio.middleware.sinatra());
-
 backboneio.listen(app, { todos: backend });
-
-/*
-app.post('/sync.json', function (req, res) {
-  console.log("in sync.json...");
-  io.sockets.emit('sync', req.body);
-  res.sendfile(__dirname + '/sync.html');
-});
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-
-  socket.on("sync", function (data) {
-    console.log('in sync event');
-    io.sockets.emit('sync', data);
-  });
-
-  socket.on("after connect", function () {
-    console.log('hello...');
-    console.log(arguments);
-  });
-});
-*/
