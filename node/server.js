3ad30f4a8f7f00e19eb9a552d2badafc1ee709ca
Michal Kuklis <michal.kuklis@gmail.com>
2012-06-22 12:46:30 -0400

    adding express

--- node/server.js
+++ node/server.js
@@ -1,8 +1,25 @@
-var io = require('socket.io').listen(8080);
+var express = require('express')
+  ,  socket = require('socket.io')
+  , app = express.createServer();
+
+app.listen(8080);
+app.use(express.bodyParser());
+io = socket.listen(app);
+
+app.post('/sync.json', function (req, res) {
+  console.log(req.body);
+  socket.emit('news', { hello: 'from sync' });
+  res.sendfile(__dirname + '/sync.html');
+});
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
+  });
+
+  socket.on("after connect", function () {
+    console.log('hello...');
+    console.log(arguments);
   });
 });
