be2ee6660dd8648bbfc37fbeed3c5dcd643df019
Michal Kuklis <michal.kuklis@gmail.com>
2012-06-23 01:41:30 -0400

    more cleanup

--- node/server.js
+++ node/server.js
@@ -1,40 +1,10 @@
 var http = require('http');
 var backboneio = require('backbone.io');
-var rest = require('restler');
+var rails = require('./middleware/rails');
 var app = http.createServer();
 
 app.listen(3000);
 
 var backend = backboneio.createBackend();
-
-backboneio.middleware.sinatra = function () {
-  var uri = 'http://10.0.0.14:4567';
-
-  return function(req, res, next) {
-    console.log(req.backend);
-    var crud = {
-      create: function () {
-        req.model.node = true;
-        rest.postJson(uri + '/' + req.backend, req.model).on('complete', function (data, response) {
-          console.log(data);
-          res.end(data);
-        });
-      },
-      read: function () {},
-      update: function () {
-        /*
-        rest.put(uri + '/todo', req.model.id).on('complete', function (data, response) {
-          res.end(data);
-        });
-        */
-      },
-      delete: function () {}
-    };
-
-    if (!crud[req.method]) return next(new Error('Unsuppored method ' + req.method));
-    crud[req.method]();
-  }
-}
-
-backend.use(backboneio.middleware.sinatra());
+backend.use(rails('http://localhost:4567'));
 backboneio.listen(app, { procedures: backend, todo: backend });
