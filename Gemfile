428db4d9672ed9e91df28d7f69d7f3e9efdcf337
Michal Kuklis <michal.kuklis@gmail.com>
2012-06-23 15:24:10 -0400

    more work on rails middleware

--- Gemfile
+++ Gemfile
@@ -4,7 +4,8 @@ gem "sinatra"
 gem "rest-client"
 gem "json"
 #gem "socket.io", :path => "~/workspace-aptana/socket.io-ruby"
-#gem "socketio-client", :path => "~/workspace-aptana/socket.io-ruby-client"
+gem "socketio-client"
+#:path => "~/workspace-aptana/socket.io-ruby-client"
 
 group :development do
   gem "shotgun"
--- node/middleware/rails.js
+++ node/middleware/rails.js
@@ -1,27 +1,28 @@
 var rest = require('restler');
 
 var rails = function (url) {
+
+  // Map from CRUD to HTTP
+  var methodMap = {
+    'create': 'post',
+    'update': 'put',
+    'delete': 'del',
+    'read':   'get'
+  };
+
   return function(req, res, next) {
-    var crud = {
-      create: function () {
-        req.model.node = true;
-        rest.postJson(url + '/' + req.backend, req.model).on('complete', function (data, response) {
-          res.end(data);
-        });
-      },
-      read: function () {},
-      update: function () {
-        /*
-        rest.put(uri + '/todo', req.model.id).on('complete', function (data, response) {
-          res.end(data);
-        });
-        */
-      },
-      delete: function () {}
+
+    if (!methodMap[req.method]) return next(new Error('Unsuppored method ' + req.method));
+
+    var options = {
+      method: methodMap[req.method]
     };
 
-    if (!crud[req.method]) return next(new Error('Unsuppored method ' + req.method));
-    crud[req.method]();
+    var path = (req.model.id) ? '/' + req.backend + '/' + req.model.id : '/' + req.backend;
+
+    rest.json(url + path, req.model, options)
+      .on("success", res.end)
+      .on("error", next);
   }
 }
 
